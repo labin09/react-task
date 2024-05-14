55a90d3940f9a0b2cca4ca1a2c0d3463eb294d50
labinblog <labinsimkhada123@gmail.com>
2024-05-14 18:56:52 +0545

    first commit

---
+++ todos-server/server.js
@@ -0,0 +1,22 @@
+const express =require("express")
+var cors = require('cors')
+const app=express();
+
+app.use(express.json());
+app.use(cors())
+let todos =["html","css","js","react"];// simulating database
+
+app.get("/api/todos", (req,res)=>{
+    res.send(todos)
+});  
+
+app.post("/api/todos", (req,res)=>{
+    todos.push(req.body.title);
+    res.send(todos);
+});
+
+
+app.listen(8000,()=>{
+console.log("server started");
+console.log("server started");
+});
